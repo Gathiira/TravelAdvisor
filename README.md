696a982762c160733b5b45e0874f2b32493f8444
Gathiira <gathiiramwangi@gmail.com>
2022-10-22 12:28:46 +0300

    header and map ui

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Travel Advisor
 
 - Travel advisor project
+
+<!-- placeholder image -->
+<https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg>
+
+<http://openweathermap.org/img/w/$1232323.png>
+
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import { Rating } from '@material-ui/lab'
+
+import useStyles from './styles'
 
 const Map = () => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width:600px)')
+
+    const defaultProps = {
+        center: {
+            lat: -1.286389,
+            lng: 36.817223
+        },
+        zoom: 15
+    };
+
     return (
-        <div>Map</div>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyB3CjkwyK4IW2OeolzoRzZQkd5O2PbhRxM' }}
+                defaultCenter={defaultProps.center}
+                center={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={() => { }}
+                onChildClick={() => { }}
+            >
+            </GoogleMapReact>
+        </div>
     )
 }
 
